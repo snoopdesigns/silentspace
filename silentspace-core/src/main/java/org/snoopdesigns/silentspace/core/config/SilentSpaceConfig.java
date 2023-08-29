20d8e2d9582ac1e8f2f005bd6c2a7d97e086b183
Dmitry <snoopdesigns@gmail.com>
2023-08-29 14:39:14 +0200

    Fixed window size

--- silentspace-core/src/main/java/org/snoopdesigns/silentspace/core/config/SilentSpaceConfig.java
+++ silentspace-core/src/main/java/org/snoopdesigns/silentspace/core/config/SilentSpaceConfig.java
@@ -1,6 +1,5 @@
 package org.snoopdesigns.silentspace.core.config;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -16,14 +15,14 @@ public class SilentSpaceConfig {
         Properties props = null;
         try {
             props = new Properties();
-            props.load(new FileInputStream(new File("config/config.ini")));
-        } catch(Exception e) {
-            e.printStackTrace();
+            props.load(new FileInputStream("config/config.ini"));
+        } catch (Exception e) {
+            System.out.println("config.ini not found, using defaults");
         }
 
         BG_SCROLL_SPEED = Integer.valueOf(props.getProperty("BG_SCROLL_SPEED", "70"));
         GAME_WINDOW_HEIGHT = Integer.valueOf(props.getProperty("GAME_WINDOW_HEIGHT", "480"));
-        GAME_WINDOW_WIDTH = Integer.valueOf(props.getProperty("GAME_WINDOW_WIDTH", "640"));
+        GAME_WINDOW_WIDTH = Integer.valueOf(props.getProperty("GAME_WINDOW_WIDTH", "440"));
         SHIP_MOVE_SPEED = Integer.valueOf(props.getProperty("SHIP_MOVE_SPEED", "200"));
         System.out.println("BG_SCROLL_SPEED: " + BG_SCROLL_SPEED);
         System.out.println("GAME_WINDOW_HEIGHT: " + GAME_WINDOW_HEIGHT);
